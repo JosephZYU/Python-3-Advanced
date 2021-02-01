865ada3df83d1e3b5698350f24ec987e8c6df91e
JosephYu <josephyu.careers@outlook.com>
2021-02-01 14:11:23 +0800

    19

---
+++ 02.Context Managers - Efficiently Managing Resources_3.1.py
@@ -0,0 +1,6 @@
+# Jan 19 2020
+
+# with statement -> context manager
+with open('sample.txt', 'w') as f:
+    f.write('Keep it up Joseph! x 2')
+    print('Code 200')
---
+++ 19.Clarifying the Issues with Mutable Default Arguments.py
@@ -0,0 +1,6 @@
+def add_employee(emp, emp_list=[]):
+    emp_list.append(emp)
+    print(emp_list)
+
+
+emps = ['John', 'Jane']
