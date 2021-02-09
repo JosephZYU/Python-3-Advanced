62126bd88c8ee9435e1252182b9ff82de83429c8
JosephYu <josephyu.careers@outlook.com>
2021-02-09 22:59:02 +0800

    20

--- 20.Calling External Commands Using the Subprocess Module ⭐️ 路径内的所有文件名存入txt文本_2.py
+++ 20.Calling External Commands Using the Subprocess Module ⭐️ 路径内的所有文件名存入txt文本_2.py
@@ -1,3 +1,29 @@
 import subprocess
 
-subprocess.run('ls -la', shell=True)
+
+def create_project(full_path='/Users/josephyu/Desktop/', project_name='new_project'):
+    msg = f"cd {full_path} && python -m venv {project_name}/venv && cd {project_name} && touch script.py && source /venv/bin/activate && code ."
+    # msg = f'cd {full_path} && python -m venv {project_name}/venv && source {full_path}{project_name}/venv/bin/activate && {full_path}{project_name}/venv/bin/python3 -m pip install --upgrade pip && code {full_path}{project_name}'
+    subprocess.run(msg, shell=True)
+
+
+create_project(project_name='JosephYu')
+
+
+"""
+full_path = '/Users/josephyu/Desktop/'
+project_name = 'new_project'
+
+msg = f'cd {full_path} && python -m venv {project_name}/venv && source {project_name}/venv/bin/activate && {full_path}{project_name}/venv/bin/python3 -m pip install --upgrade pip && code {full_path}{project_name}'
+
+subprocess.run(msg, shell=True)
+
+class Manager(Employee):
+    def __init__(self, first, last, pay, staffs=None):
+        # 🧠 default arguments = None
+        super().__init__(first, last, pay)
+        if not staffs:
+            self.staffs = []
+        else:
+            self.staffs = staffs
+"""
